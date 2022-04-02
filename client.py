26a93ae3df2c678d08c6c1d037bad0acce71e631
ffletcherr <homayouneemoradi@gmail.com>
2022-04-03 04:03:23 +0430

    add addresses

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import requests
 # prepare headers for http request
 content_type = 'image/jpeg'
 headers = {'content-type': content_type}
-URL = "http://localhost:5000/liveness"
+URL = "http://localhost:5000/main" # main, liveness, identity
 def post_image(img_file):
     """ post image and return the response """
     img = open(img_file, 'rb').read()
