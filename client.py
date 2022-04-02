6b40e577569f74dbc6959f724f9510e6af5efa40
ffletcherr <homayouneemoradi@gmail.com>
2022-04-03 03:58:39 +0430

    add flask client

---
+++ client.py
@@ -0,0 +1,12 @@
+import requests
+# prepare headers for http request
+content_type = 'image/jpeg'
+headers = {'content-type': content_type}
+URL = "http://localhost:5000/liveness"
+def post_image(img_file):
+    """ post image and return the response """
+    img = open(img_file, 'rb').read()
+    response = requests.post(URL, data=img, headers=headers)
+    return response.json()
+
+response = post_image("data/test.jpg")
