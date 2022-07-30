84fae5e6937a797f2be055344500259b6cd6c4c6
ffletcherr <homayouneemoradi@gmail.com>
2022-07-30 23:41:44 +0430

    add setup.py for facetools

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="facetools",
+    version="0.1",
+    keywords="face_detection, face_recognition, liveness_detection",
+    url="",
+    description="face tools",
+    packages=["facetools"],
+)
